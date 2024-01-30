03bb03f7c96aa8872e264bbdf70c219c38ec64f6
frojak4 <Frojak4@gmail.com>
2024-01-30 09:47:42 +0100

    Farge blir grønn når man vinner

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let forsøk;
 
 function mainScreen() {
     forsøk = 0;
+    document.documentElement.style.backgroundImage = "linear-gradient(-20deg, #100259 0%, #365473 100%)";
     randomNumber = Math.floor(Math.random() * 10)
     app.innerHTML = /*HTML*/ `
     <h1 class="overskrift"> Gjett Nummer</h1>
@@ -35,10 +36,13 @@ function gjett() {
     `
 } else if (gjettaNummer == randomNumber) {
     forsøk ++;
+    document.documentElement.style.backgroundImage = "linear-gradient(-20deg, #01490b 0%, #00f014 100%)";
     app.innerHTML += /*HTML*/ `
     <h1 class="skrift">Gratulerer! Du klarte det på ${forsøk}. forsøk.</h1> <br/>
     <button onclick = mainScreen()>Spel igjen?</button
     `
+
 } 
 }
+
 
